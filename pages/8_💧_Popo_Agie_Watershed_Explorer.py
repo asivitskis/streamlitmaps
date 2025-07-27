94a6ddd7712c8d65cf295a102c8ec3e4fc8297c9
asivitskis <sivitskis.alex@gmail.com>
2025-07-27 10:50:32 -0600

    Update 8_💧_Popo_Agie_Watershed_Explorer.py
    Update watershed viewer with python code.

--- pages/8_💧_Popo_Agie_Watershed_Explorer.py
+++ pages/8_💧_Popo_Agie_Watershed_Explorer.py
@@ -1,1 +1,27 @@
+import streamlit as st
+import leafmap.foliumap as leafmap
 
+st.set_page_config(layout="wide")
+
+markdown = """
+A Streamlit map template
+<https://github.com/opengeos/streamlit-map-template>
+"""
+
+st.sidebar.title("About")
+st.sidebar.info(markdown)
+logo = "https://i.imgur.com/UbOXYAU.png"
+st.sidebar.image(logo)
+
+st.title("Marker Cluster")
+
+with st.expander("See source code"):
+    with st.echo():
+
+        m = leafmap.Map(center=[40, -100], zoom=4)
+        regions = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_regions.geojson"
+        streams = "https://raw.githubusercontent.com/asivitskis/EarthInquiryLab/main/data/Hydro_data/stream_network.geojson"
+
+        m.add_geojson(streams, layer_name="streams")
+
+m.to_streamlit(height=700)
