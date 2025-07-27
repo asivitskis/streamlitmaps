81bd51f50d8e330417736add224d372b82f0b3b0
asivitskis <sivitskis.alex@gmail.com>
2025-07-27 11:03:26 -0600

    Update 8_💧_Popo_Agie_Watershed_Explorer.py
    Updated layers and styles.

--- pages/8_💧_Popo_Agie_Watershed_Explorer.py
+++ pages/8_💧_Popo_Agie_Watershed_Explorer.py
@@ -21,10 +21,15 @@ with st.expander("Popo Agie Watershed Explorer"):
         hillshade = "https://github.com/asivitskis/EarthInquiryLab/raw/refs/heads/main/data/Elevation/hillshade_cog.tif"
         smoothed_dem = "https://github.com/asivitskis/EarthInquiryLab/raw/refs/heads/main/data/Elevation/smoothed_dem_cog.tif"
         streams = "https://raw.githubusercontent.com/asivitskis/EarthInquiryLab/main/data/Hydro_data/stream_network.geojson"
+        style = {"color": "black", "weight": 3, "opacity": 1}
         
         m = leafmap.Map(center=[40, -100], zoom=4)
+        m.add_cog_layer(smoothed_dem, name="Smoothed DEM")
         m.add_cog_layer(hillshade, name="Hillshade COG", layer_opacity=0.8)
-        m.add_cog_layer(smoothed_dem, layer_name="Smoothed DEM")
-        m.add_geojson(streams, layer_name="streams")
+        m.add_geojson(
+            streams,
+            layer_name="streams",
+            hover_style=style,
+        )
 
 m.to_streamlit(height=700)
