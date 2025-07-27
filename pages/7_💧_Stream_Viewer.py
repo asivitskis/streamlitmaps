0b2e1831cfdc3f45d2c8eddb7dde9a5a09908e69
asivitskis <sivitskis.alex@gmail.com>
2025-07-27 14:09:19 -0600

    Update 7_💧_Stream_Viewer.py

--- pages/7_💧_Stream_Viewer.py
+++ pages/7_💧_Stream_Viewer.py
@@ -16,7 +16,7 @@ st.title("Interactive Map")
 
 col1, col2 = st.columns([4, 1])
 options = list(leafmap.basemaps.keys())
-index = options.index("Satellite")
+index = options.index("OpenTopoMap")
 
 with col2:
 
@@ -24,28 +24,9 @@ with col2:
 
 
 with col1:
-    hillshade = "https://github.com/asivitskis/EarthInquiryLab/raw/refs/heads/main/data/Elevation/hillshade_cog.tif"
-    smoothed_dem = "https://github.com/asivitskis/EarthInquiryLab/raw/refs/heads/main/data/Elevation/smoothed_dem_cog.tif"
-    basin = "https://raw.githubusercontent.com/asivitskis/EarthInquiryLab/refs/heads/main/data/Hydro_data/pa_HUC10_basin.geojson"
-    streams = "https://raw.githubusercontent.com/asivitskis/EarthInquiryLab/main/data/Hydro_data/stream_network.geojson"
-    hstyle = {"color": "black", "weight": 3, "opacity": 1}
-    
+
     m = leafmap.Map(
         locate_control=True, latlon_control=True, draw_export=True, minimap_control=True
     )
     m.add_basemap(basemap)
-    m.add_cog_layer(smoothed_dem, name="Smoothed DEM", palette="terrain")
-        m.add_cog_layer(hillshade, name="Hillshade COG", opacity=0.2)
-        m.add_geojson(
-            basin,
-            layer_name="HUC 10 Basin",
-            style={"color": "black", "weight": 2, "fillOpacity": 0},
-            info_mode="on_click",
-        )
-        m.add_geojson(
-            streams,
-            layer_name="Drainage Network",
-            style={"color": "#ff2a00", "weight": 2},
-            hover_style=hstyle,
-        )
     m.to_streamlit(height=700)
