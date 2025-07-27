8a7d32c5dfa2171a4ddfe9e85f096a222f40fffc
asivitskis <sivitskis.alex@gmail.com>
2025-07-27 10:41:49 -0600

    Update 7_💧_Stream_Viewer.py
    Removing cluster.

--- pages/7_💧_Stream_Viewer.py
+++ pages/7_💧_Stream_Viewer.py
@@ -19,18 +19,8 @@ with st.expander("See source code"):
     with st.echo():
 
         m = leafmap.Map(center=[40, -100], zoom=4)
-        cities = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv"
         regions = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_regions.geojson"
 
         m.add_geojson(regions, layer_name="US Regions")
-        m.add_points_from_xy(
-            cities,
-            x="longitude",
-            y="latitude",
-            color_column="region",
-            icon_names=["gear", "map", "leaf", "globe"],
-            spin=True,
-            add_legend=True,
-        )
 
 m.to_streamlit(height=700)
